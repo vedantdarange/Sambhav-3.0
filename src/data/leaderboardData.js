dd3f1a6436c8854536f4d269f69c3e69eccc67c5
Vedant <vedantdarange2327@gmail.com>
2026-02-03 23:28:38 +0530

    Update leaderboard data

--- src/data/leaderboardData.js
+++ src/data/leaderboardData.js
@@ -12,7 +12,7 @@ export const leaderboardData = [
         id: "cse",
         tag: "CSE",
         name: "Computer Science & Engineering",
-        points: 0,
+        points: 100,
         img: cse,
     },
     {
@@ -47,7 +47,7 @@ export const leaderboardData = [
         id: "mech",
         tag: "MECH",
         name: "Mechanical Engineering",
-        points: 160,
+        points: 260,
         img: mech,
     },
     {
